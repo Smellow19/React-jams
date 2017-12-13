0d76bf9931b26363b61e6d370be24744a3e96beb
Tyler <Tylerwbridges@gmail.com>
2017-12-12 20:28:25 -0600

    Routes made!

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import {Route, Link} from 'react-router-dom';
 import './App.css';
+import Album from './components/Album';
+import Landing from './components/Landing'
+import Library from './components/Library'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       <header>
+        <nav>
+          <Link to='/'> Landing </Link>
+          <Link to ='/Library'> Library </Link>
+        </nav>
         <h1> Bloc Jams </h1>
       </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <main>
+      <Route exact path="/" component={Landing}/>
+      <Route path ="/library" component={Library}/>
+      <Route path ="/album" component={Album}/>
+      </main>
       </div>
     );
   }
